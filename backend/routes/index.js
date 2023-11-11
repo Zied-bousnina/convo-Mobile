2a9065f86eef3f9a8cbab700f27119f0b79430e6
Zied-bousnina <zbousnina@yahoo.com>
2023-11-11 09:21:29 +0100

    I have delet the backend folder from the mobile project

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const {  registerUser } = require('../controllers/users.controller');
-var router = express.Router();
-
-/* GET home page. */
-router.post('/register',registerUser );
-
-module.exports = router;
